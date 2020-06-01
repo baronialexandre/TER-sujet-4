991a0e5c694940970123905c374b34606b6dda4a
baronialexandre <alexandre.baroni.1@etu.univ-amu.fr>
2020-06-01 13:53:03 +0200

    spring dao chemin relatif

--- src/springapp/dao/SpringDAOConfiguration.java
+++ src/springapp/dao/SpringDAOConfiguration.java
@@ -21,60 +21,56 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class SpringDAOConfiguration {
 
-    /*
-     * Définition de la source de données
-     */
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUsername("bandol");
-        dataSource.setPassword("bandol");
-        //dataSource.setUrl("jdbc:h2:file:.\\data_TER_04\\h2db"); // A activer si .war
-        dataSource.setUrl("jdbc:h2:file:D:\\Master\\Ter\\data_TER_04\\h2db"); // A modifier localement pour dev, ne pas commit ce fichier (une fois modif) :)
-        return dataSource;
-    }
+	/*
+	 * Définition de la source de données
+	 */
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUsername("bandol");
+		dataSource.setPassword("bandol");
+		dataSource.setUrl("jdbc:h2:file:./data_TER_04/h2db");
+		return dataSource;
+	}
 
-    /*
-     * Construction de l'EMF à partir de la source de données et du
-     * choix d'hibernate. Cette configuration remplace le fichier
-     * persistence.xml
-     */
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-    	LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(dataSource());
-        em.setPackagesToScan(new String[] { "springapp.model" });
-        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-        // Configuration d'hibernate
-        Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", "update"); //create-drop pour drop à la fin du test ou update
-        properties.setProperty("hibernate.dialect", //
-                "org.hibernate.dialect.H2Dialect");
-        properties.setProperty("hibernate.show_sql", "false");
-        properties.setProperty("hibernate.format_sql", "true");
-        em.setJpaProperties(properties);
-        return em;
-    }
+	/*
+	 * Construction de l'EMF à partir de la source de données et du choix
+	 * d'hibernate. Cette configuration remplace le fichier persistence.xml
+	 */
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(dataSource());
+		em.setPackagesToScan(new String[] { "springapp.model" });
+		em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+		// Configuration d'hibernate
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", "update"); // create-drop pour drop à la fin du test ou update
+		properties.setProperty("hibernate.dialect", //
+				"org.hibernate.dialect.H2Dialect");
+		properties.setProperty("hibernate.show_sql", "false");
+		properties.setProperty("hibernate.format_sql", "true");
+		em.setJpaProperties(properties);
+		return em;
+	}
 
-    /*
-     * Construction d'un gestionnaire de transaction
-     * en liaison avec l'usine à EM.
-     */
-    @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(emf);
-        return transactionManager;
-    }
+	/*
+	 * Construction d'un gestionnaire de transaction en liaison avec l'usine à EM.
+	 */
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(emf);
+		return transactionManager;
+	}
 
-    /*
-     * Activer le traitement des annotations
-     * de gestion du contexte de persistence.
-     */
-    @Bean
-    public PersistenceAnnotationBeanPostProcessor annotationProcessor() {
-        return new PersistenceAnnotationBeanPostProcessor();
-    }
+	/*
+	 * Activer le traitement des annotations de gestion du contexte de persistence.
+	 */
+	@Bean
+	public PersistenceAnnotationBeanPostProcessor annotationProcessor() {
+		return new PersistenceAnnotationBeanPostProcessor();
+	}
 
 }
